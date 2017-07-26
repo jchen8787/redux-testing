18452134b1074f90210ae97a1e3c65f876e11930
Justin Chen <jchen@fetchrobotics.com>
2017-07-26 13:35:23 -0700

    finished redux tutorial 3

---
+++ src/js/client.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+
+const reducer = function(state, action) {
+  if (action.type === "INC") {
+    return state + action.payload;
+  }
+  if (action.type === "DEC") {
+    return state - action.payload;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 0);
+
+store.subscribe(() => {
+  console.log("store changed", store.getState());
+})
+
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "INC", payload: 5});
+store.dispatch({type: "DEC", payload: 8});
+store.dispatch({type: "INC", payload: 235});
+store.dispatch({type: "INC", payload: 1});
+store.dispatch({type: "DEC", payload: 1666});
